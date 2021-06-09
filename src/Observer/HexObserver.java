87da5592ef411c7c28c987280c7c557390316045
MegamanXD <chick.litttle007@gmail.com>
2021-06-09 20:30:29 +0700

    Decorator, Memento, Observer, Adapter, Prototype

---
+++ src/Observer/HexObserver.java
@@ -0,0 +1,15 @@
+package Observer;
+
+public class HexObserver extends Observer{
+    private Subject subject;
+
+    public HexObserver(Subject subject){
+        this.subject = subject;
+        this.subject.attach(this);
+    }
+
+    @Override
+    public void update() {
+        System.out.println( "Hex String: " + Integer.toHexString( subject.getState() ).toUpperCase() );
+    }
+}
